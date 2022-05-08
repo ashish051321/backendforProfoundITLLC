a2b270b86bad9ed891d6e230e374e94a58eed3d8
ashish051321 <ashish.25jl@gmail.com>
2022-05-08 17:45:19 +0530

    feature2: Adding email sending capability using spring-boot-starter-mail

---
+++ src/main/java/com/profounditllc/backendforwebsite/configuration/MailConfiguration.java
@@ -0,0 +1,48 @@
+package com.profounditllc.backendforwebsite.configuration;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfiguration {
+	
+	@Value("${spring.mail.host}")
+	private String host;
+	
+	@Value("${spring.mail.port}")
+	private int port;
+	
+	@Value("${spring.mail.username}")
+	private String username;
+	
+	@Value("${spring.mail.password}")
+	private String password;
+	
+	@Value("${mail.debug}")
+	private String mailDebug;
+	
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost(host);
+	    mailSender.setPort(port);
+	    
+	    mailSender.setUsername(username);
+	    mailSender.setPassword(password);
+	    
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", mailDebug);
+	    
+	    return mailSender;
+	}
+	
+}
--- src/main/java/com/profounditllc/backendforwebsite/controllers/EmailController.java
+++ src/main/java/com/profounditllc/backendforwebsite/controllers/EmailController.java
@@ -1,6 +1,9 @@
 package com.profounditllc.backendforwebsite.controllers;
 
 import com.profounditllc.backendforwebsite.models.communication.ServerResponse;
+import com.profounditllc.backendforwebsite.services.EmailService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmailController {
 
+	EmailService emailService;
+	
+	@Autowired
+	public EmailController(EmailService emailService) {
+		this.emailService = emailService;
+	}
+	
     @GetMapping("/checksanity")
     public ResponseEntity<ServerResponse> checkSanity() {
+    	this.emailService.sendSimpleMessage("ashish.25jl@gmail.com", "Test Spring Boot Mailer", "Hello from Profound IT LLC Careers.");
         return new ResponseEntity<>(new ServerResponse("Things look fine", "Object content"), HttpStatus.OK);
     }
 }
