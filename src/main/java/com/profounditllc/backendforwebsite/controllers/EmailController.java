b2bccd9862c2824f8204966e7c19a5b6f47cdf22
ashish051321 <ashish.25jl@gmail.com>
2022-05-14 10:28:58 +0530

    feature2 : able to send email with attachments

--- src/main/java/com/profounditllc/backendforwebsite/controllers/EmailController.java
+++ src/main/java/com/profounditllc/backendforwebsite/controllers/EmailController.java
@@ -1,12 +1,22 @@
 package com.profounditllc.backendforwebsite.controllers;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.profounditllc.backendforwebsite.models.communication.ServerResponse;
 import com.profounditllc.backendforwebsite.models.email.EmailRequest;
 import com.profounditllc.backendforwebsite.services.EmailService;
@@ -14,7 +24,8 @@ import com.profounditllc.backendforwebsite.services.EmailService;
 @RestController
 public class EmailController {
 
-	EmailService emailService;
+	private EmailService emailService;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	public EmailController(EmailService emailService) {
@@ -31,10 +42,23 @@ public class EmailController {
 		return new ResponseEntity<>(new ServerResponse("Things look fine", "Object content"), HttpStatus.OK);
 	}
 
-	@PostMapping("/sendMailWithAttachments")
-	public ResponseEntity<ServerResponse> sendMailWithAttachments(@RequestBody EmailRequest emailRequest) {
-		this.emailService.sendSimpleMail("ashish.25jl@gmail.com", "Test Spring Boot Mailer",
-				"Hello from Profound IT LLC Careers.");
+	@PostMapping(value = "/sendMailWithAttachments", consumes = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<ServerResponse> sendMailWithAttachments(@RequestPart("mailRequest") String emailRequestText,
+			@RequestPart("file") List<MultipartFile> files) throws JsonMappingException, JsonProcessingException {
+
+		// Converting jsonString to object EmailRequest
+		// Had to use String as @RequestPart usage for accepting both a json and
+		// multipartfile requires this.
+		ObjectMapper objectMapper = new ObjectMapper();
+		EmailRequest emailRequest = objectMapper.readValue(emailRequestText, EmailRequest.class);
+
+		int fileCount = files.size();
+		logger.info("number of files received: {}", files.size());
+		logger.info("Mail request received: {}", emailRequest);
+		this.emailService.sendMailWithAttachment("ashish.25jl@gmail.com", "Test Spring Boot Mailer",
+				"Hello from Profound IT LLC Careers.", files.get(0));
 		return new ResponseEntity<>(new ServerResponse("Things look fine", "Object content"), HttpStatus.OK);
+
 	}
 }
--- src/main/java/com/profounditllc/backendforwebsite/services/EmailService.java
+++ src/main/java/com/profounditllc/backendforwebsite/services/EmailService.java
@@ -1,9 +1,15 @@
 package com.profounditllc.backendforwebsite.services;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class EmailService {
@@ -20,12 +26,20 @@ public class EmailService {
 		emailSender.send(message);
 	}
 
-	public void sendMailWithAttachment(String to, String subject, String text) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("careers@profounditllc.com");
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(text);
+	public void sendMailWithAttachment(String to, String subject, String text, MultipartFile file) {
+		MimeMessage message = emailSender.createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+			helper.setFrom("careers@profounditllc.com");
+			helper.setTo(to);
+			helper.setSubject(subject);
+			helper.setText(text);
+			helper.addAttachment(file.getOriginalFilename(), file);
+
+		} catch (MessagingException ex) {
+			ex.printStackTrace();
+		}
 		emailSender.send(message);
 	}
 
